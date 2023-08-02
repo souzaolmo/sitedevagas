e821170acf0daafbcd338a165c05657fd14c7aba
Júlio Olmo <souzaolmo@gmail.com>
2023-08-02 00:38:11 -0300

    up

---
+++ src/components/comuns/IndicadorVaga.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+        <p>{{ titulo }}</p>
+        <h2>{{indicador}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'IndicadorVaga',
+    props: ['titulo', 'indicador']
+}
+</script>
---
+++ src/components/comuns/PesquisarVaga.vue
@@ -0,0 +1,25 @@
+<template>
+            <div class="h-100 p-5 bg-ligth border rounded-3">
+              <h2>Pesquisar Vagas</h2>
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <label>Título da vaga</label>
+                    <input type="tex" class="form-control" placeholder="pesquise por palavras chaves, por exemplo 'PHP', 'Pelno', 'Analista'">
+                    <small class="form-text text-muted"> Informe palavras que estejam relacionadas com o titulo da vaga que você proucura</small>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <button class="btn btn-outline-dark mt2" type="button">Buscar</button>
+                </div>
+              </div>
+            </div>
+</template>
+
+<script>
+export default {
+    name: 'PesquisarVaga'
+}
+</script>
