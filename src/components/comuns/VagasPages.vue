ef40b533d4b0df7f17c1a72dd777393162769a3b
Júlio Olmo <souzaolmo@gmail.com>
2023-08-10 01:10:16 -0300

    up

---
+++ src/components/comuns/VagasPages.vue
@@ -0,0 +1,17 @@
+<template>
+         <div class="card">
+          <div class="card-header bg-dark text-white">Título da vaga</div>
+          <div class="card-body">
+            <p>Descrição truncada da vaga...</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"> Salário: R$ 6.000,00 | Modalidade: Home Office | Tipo: CLT | Publicação: 10/11/2021</small>
+          </div>
+        </div>
+</template>
+
+<script> 
+export default {
+    name: 'VagasPages'
+}
+</script>
--- src/components/views/HomePage.vue
+++ src/components/views/HomePage.vue
@@ -1,36 +1,43 @@
 <template>
-    <div class="container py-4">
-        <div class="row">
-          <div class="col">
-            <pesquisar-vaga>s</pesquisar-vaga>
-          </div>  
-        </div>
-
-          <div class="row mt-5">
-            <div class="col-4">
-              <IndicadorVaga titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></IndicadorVaga>
-            </div>
-            <div class="col-4">
-              <IndicadorVaga titulo="Profissionais Cadastrados" indicador="225" bg="bg-dark" color="text-white"></IndicadorVaga>
-            </div>
-            <div class="col-4">
-              <IndicadorVaga titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-ligth" color="text-dark"></IndicadorVaga>
-              
-            </div>
-          </div>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <pesquisar-vaga>s</pesquisar-vaga>
+      </div>
     </div>
-  
+
+    <div class="row mt-5">
+      <div class="col">
+        <VagasPages/>
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-4">
+        <IndicadorVaga titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></IndicadorVaga>
+      </div>
+      <div class="col-4">
+        <IndicadorVaga titulo="Profissionais Cadastrados" indicador="225" bg="bg-dark" color="text-white"></IndicadorVaga>
+      </div>
+      <div class="col-4">
+        <IndicadorVaga titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-ligth" color="text-dark"></IndicadorVaga>
+      </div>
+    </div>
+  </div>
 </template>
+
   
   <script>
   import IndicadorVaga from '@/components/comuns/IndicadorVaga.vue'
   import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
+  import VagasPages from '@/components/comuns/VagasPages.vue'
   
   export default {
     name: 'HomePage',
     components: {
       PesquisarVaga,
-      IndicadorVaga
+      IndicadorVaga,
+      VagasPages
     },
     data: () => ({
       usuariosOnline: 0
